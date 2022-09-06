879589fafd4ce3e146d9c75c2233cb209f0c677b
Adrian Hope-Bailie <adrian@fynbos.dev>
2022-09-06 13:27:22 +0200

    fix: add digest to tes

--- tests/functional/gnap/init.spec.ts
+++ tests/functional/gnap/init.spec.ts
@@ -2,8 +2,9 @@ import { test } from '@japa/runner'
 
 test('request grant', async ({ client }) => {
   const response = await client.post('/gnap',).headers({
+    'Content-Digest' : 'sha-256=:47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=:'
     'Signature': 'sig1=:iuwefiwuef:',
-    'Signature-Input': 'sig1=(@method @target-uri content-digest);created=0;keyid=test;8weryyewr'
+    'Signature-Input': 'sig1=("@method" "@target-uri" "content-digest");created=0;keyid="test"'
   }).json({
     client: 'https://localhost:3000'
   })
