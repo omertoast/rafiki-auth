2a9c771712034a475a743e3d335a1311e700835a
Ömer Talha Özdemir <omertoast@gmail.com>
2022-09-15 14:17:57 +0300

    feat: openapi validator

--- app/Controllers/Http/Gnap/GnapController.ts
+++ app/Controllers/Http/Gnap/GnapController.ts
@@ -1,10 +1,18 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { createOpenAPI, HttpMethod } from 'openapi'
+import { operations } from '../../../../auth-open-payments-schema';
+import { OpenAPI } from 'openapi-types';
 
 // GnapController sits behind the '/gnap' route to handle the implementation of the GNAP protocol
 export default class GnapController {
 
   // https://www.ietf.org/archive/id/draft-ietf-gnap-core-protocol-10.html#name-requesting-access
-  public async requestGrant({}: HttpContextContract) {}
+  public async requestGrant(ctx: HttpContextContract) {
+    const payload = await validateGnapRequest<RequestGrantBody>(ctx)
+      .catch(errors => ctx.response.badRequest(errors))
+
+    return payload
+  }
 
   // https://www.ietf.org/archive/id/draft-ietf-gnap-core-protocol-10.html#name-continuing-a-grant-request
   public async continueGrant({}: HttpContextContract) {}
@@ -21,4 +29,28 @@ export default class GnapController {
   // https://www.ietf.org/archive/id/draft-ietf-gnap-core-protocol-10.html#name-revoking-the-access-token
   public async revokeToken({}: HttpContextContract) {}
 
+}
+
+type RequestGrantBody = operations['post']['requestBody']['content']['application/json']
+
+async function validateGnapRequest<T>(ctx: HttpContextContract): Promise<T> { 
+  const spec = await createOpenAPI('./auth-open-payments-spec.yaml')
+  const requestValidator = spec.createRequestValidator<OpenAPI.Request>({
+    path: "/",
+    method: HttpMethod.POST
+  })
+
+  const openAPIRequest: OpenAPI.Request = {
+    headers: ctx.request.headers(),
+    body: ctx.request.body(),
+    params: ctx.request.params(),
+    query: ctx.request.qs()
+  }
+
+  try {
+    const isValid = requestValidator(openAPIRequest)
+    return ctx.request.body() as T
+  } catch (errors) {
+    throw errors
+  }
 }
--- tests/functional/gnap/init.spec.ts
+++ tests/functional/gnap/init.spec.ts
@@ -1,13 +1,64 @@
 import { test } from '@japa/runner'
+import { v4 } from 'uuid'
+import { FinishMethod, StartMethod } from '../../../app/Models/Grant'
+import { AccessType, Action } from '../../../app/Models/GrantAccess'
+import * as crypto from 'crypto'
+
+export const TEST_CLIENT_DISPLAY = {
+  name: 'Test Client',
+  uri: 'https://example.com'
+}
+
+export const KEY_REGISTRY_ORIGIN = 'https://openpayments.network'
+export const KID_PATH = '/keys/base-test-key'
+
+export const TEST_CLIENT_KEY = {
+  proof: 'httpsig',
+  jwk: {
+    kid: KEY_REGISTRY_ORIGIN + KID_PATH,
+    x: 'hin88zzQxp79OOqIFNCME26wMiz0yqjzgkcBe0MW8pE',
+    kty: 'OKP',
+    alg: 'EdDSA',
+    crv: 'Ed25519',
+    key_ops: ['sign', 'verify'],
+    use: 'sig'
+  }
+}
+
+const BASE_GRANT_REQUEST = {
+  access_token: {
+    access: [
+      {
+        type: AccessType.IncomingPayment,
+        actions: [Action.Create, Action.Read, Action.List],
+        locations: ['https://example.com'],
+        identifier: `https://example.com/${v4()}`
+      }
+    ]
+  },
+  client: {
+    display: TEST_CLIENT_DISPLAY,
+    key: TEST_CLIENT_KEY
+  },
+  interact: {
+    start: [StartMethod.Redirect],
+    finish: {
+      method: FinishMethod.Redirect,
+      uri: 'https://example.com/finish',
+      nonce: crypto.randomBytes(8).toString('hex').toUpperCase()
+    }
+  }
+}
 
 test('request grant', async ({ client }) => {
   const response = await client.post('/gnap',).headers({
-    'Content-Digest' : 'sha-256=:47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=:',
-    'Signature': 'sig1=:iuwefiwuef:',
-    'Signature-Input': 'sig1=("@method" "@target-uri" "content-digest");created=0;keyid="key-1"'
-  }).json({
-    client: 'https://localhost:3000'
-  })
+    'Content-Type': 'application/json',
+    'Accepts': 'application/json'
+  }).json(
+    BASE_GRANT_REQUEST
+  )
+
+  console.log(response.body())
+
   response.assertStatus(200)
-  // response.assertBodyContains({ hello: 'world' })
 })
